f8697507ef4648de3b092decd080b94b35601452
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-05 19:55:01 -0500

    completed silicon valley

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,53 @@
 class Startup
+  attr_accessor :name, :founder, :domain
+  @@all = []
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    self.class.all << self
+  end
+
+  def pivot(new_domain, new_name)
+    self.domain = new_domain
+    self.name = new_name
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    sign = FundingRound.new(self, venture_capitalist, type, investment)
+  end
+
+  def num_funding_rounds
+    all_funding_rounds.size
+  end
+
+  def total_funds
+    all_funding_rounds.sum{|f|f.investment}
+  end
+
+  def investors
+    all_funding_rounds.map{|f| f.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    funding = all_funding_rounds.select{|f| f.venture_capitalist.tres_commas_club == "Y"}
+    funding.map{|fd|fd.venture_capitalist}.uniq
+  end
+
+  def all_funding_rounds
+    FundingRound.all.select{|f| f.startup == self}
+  end
+
+  def self.find_by_founder(founder)
+    self.all.find{|stp|stp.founder == founder}
+  end
+
+  def self.domains
+    self.all.map{|stp| stp.domain}
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,47 @@
 class VentureCapitalist
+  attr_accessor :name, :total_worth, :tres_commas_club
+  @@all = []
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    if @total_worth > 1000000000.0
+      @tres_commas_club = "Y"
+    else
+      @tres_commas_club = "N"
+    end
+    self.class.all << self
+  end
 
+  def offer_contract(startup, type, investment)
+    sign = FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    all_funding_rounds
+  end
+
+  def portfolio
+    all_funding_rounds.map{|f| f.startup}.uniq
+  end
+
+  def biggest_investment
+    funding = all_funding_rounds
+    funding.max_by{|f|f.investment}
+  end
+
+  def invested(domain)
+    all_funding_rounds.select{|f|f.startup.domain == domain}.sum{|s|s.investment}
+  end
+
+  def all_funding_rounds
+    FundingRound.all.select{|f| f.venture_capitalist == self}
+  end
+
+  def self.tres_commas_club
+    self.all.select{|vc|vc.tres_commas_club == "Y"}
+  end
+
+  def self.all
+    @@all
+  end
 end
